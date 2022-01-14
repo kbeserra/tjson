ba3ce3a2b2886fbe8f5fd5b32c8c67d6fcee2abd
Kyle Beserra <kylebeserra@gmail.com>
2022-01-14 18:09:47 -0500

    update

--- tjson.go
+++ tjson.go
@@ -20,8 +20,6 @@ func RegisterType(key string, value func() interface{}) {
 	}
 
 	typeMap[key] = value
-
-	fmt.Println(typeMap)
 }
 
 func GetFromType(key string) (interface{}, error) {
@@ -32,7 +30,7 @@ func GetFromType(key string) (interface{}, error) {
 	}
 }
 
-func newFromTypedObject(data []byte) (interface{}, error) {
+func NewFromTypedObject(data []byte) (interface{}, error) {
 	type typedJsonObject struct {
 		Type string `json:"type"`
 	}
@@ -45,10 +43,107 @@ func newFromTypedObject(data []byte) (interface{}, error) {
 	return GetFromType(typedObject.Type)
 }
 
-func parseHelper(data []byte, x interface{}, tag string,
-	new func([]byte, reflect.Type) (interface{}, error),
-	convert func(interface{}, reflect.Type) reflect.Value,
-) error {
+func ParseJsonTags(data []byte, x interface{}) error {
+	t := reflect.TypeOf(x).Elem()
+	V := reflect.ValueOf(x).Elem()
+	n := t.NumField()
+
+	raw := map[string]json.RawMessage{}
+	if err := json.Unmarshal(data, &raw); err != nil {
+		return err
+	}
+
+	for i := 0; i < n; i++ {
+		f := t.Field(i)
+		jsonKey := strings.Split(f.Tag.Get(JSON), ",")[0]
+
+		if r, in := raw[jsonKey]; in {
+
+			v := reflect.New(f.Type).Interface()
+
+			if err := json.Unmarshal(r, v); err != nil {
+				return err
+			}
+			V.Field(i).Set(reflect.ValueOf(v).Elem())
+
+		}
+
+	}
+
+	return nil
+}
+
+func unpack(b []byte, v reflect.Value) error {
+
+	switch v.Type().Kind() {
+	case reflect.Interface:
+		return unpackInterface(b, v)
+	case reflect.Slice:
+		return unpackSlice(b, v)
+	case reflect.Map:
+		return unpackMap(b, v)
+	}
+
+	return nil
+}
+
+func unpackInterface(b []byte, v reflect.Value) error {
+	x, err := NewFromTypedObject(b)
+
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(b, x); err != nil {
+		return err
+	}
+	v.Set(reflect.ValueOf(x).Convert(v.Type()))
+	return nil
+}
+
+func unpackSlice(b []byte, v reflect.Value) error {
+
+	var c []json.RawMessage
+	if err := json.Unmarshal(b, &c); err != nil {
+		return err
+	}
+
+	v.Set(reflect.MakeSlice(v.Type(), len(c), len(c)))
+
+	for i, raw := range c {
+		if err := unpack(raw, v.Index(i)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func unpackMap(b []byte, v reflect.Value) error {
+
+	var c map[string]json.RawMessage
+	if err := json.Unmarshal(b, &c); err != nil {
+		return err
+	}
+
+	v.Set(reflect.MakeMapWithSize(v.Type(), 0))
+
+	for rawKey, rawValue := range c {
+		key := reflect.New(v.Type().Key()).Interface()
+		if err := json.Unmarshal([]byte(rawKey), &key); err != nil {
+			return err
+		}
+
+		u := reflect.New(v.Type().Elem()).Elem()
+		unpack(rawValue, u)
+
+		v.SetMapIndex(reflect.ValueOf(key).Elem(), u)
+	}
+
+	return nil
+}
+
+func ParseTJsonTags(data []byte, x interface{}) error {
 
 	t := reflect.TypeOf(x).Elem()
 	V := reflect.ValueOf(x).Elem()
@@ -61,36 +156,15 @@ func parseHelper(data []byte, x interface{}, tag string,
 
 	for i := 0; i < n; i++ {
 		f := t.Field(i)
-		jsonKey := strings.Split(f.Tag.Get(tag), ",")[0]
+		jsonKey := strings.Split(f.Tag.Get(TJSON), ",")[0]
 
 		if r, in := raw[jsonKey]; in {
-
-			v, err := new(r, f.Type)
-			if err != nil {
+			if err := unpack(r, V.Field(i)); err != nil {
 				return err
 			}
-
-			if err := json.Unmarshal(r, v); err != nil {
-				return err
-			}
-			V.Field(i).Set(convert(v, f.Type))
 		}
 
 	}
 
 	return nil
-}
-
-func ParseJsonTags(data []byte, x interface{}) error {
-	return parseHelper(data, x, JSON,
-		func(b []byte, t reflect.Type) (interface{}, error) { return reflect.New(t).Interface(), nil },
-		func(v interface{}, t reflect.Type) reflect.Value { return reflect.ValueOf(v).Elem() },
-	)
-}
-
-func ParseTJsonTags(data []byte, x interface{}) error {
-	return parseHelper(data, x, TJSON,
-		func(b []byte, t reflect.Type) (interface{}, error) { return newFromTypedObject(b) },
-		func(v interface{}, t reflect.Type) reflect.Value { return reflect.ValueOf(v).Convert(t) },
-	)
 }
